d7c4fe8fe22893b2833b155f259baa5eb2c22c36
Aliaksandr Datskevich <datskevich.a@icloud.com>
2023-02-13 22:48:02 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let res = '';
+  n = n.toString();
+  if (n[0] === '-') {
+    n = n.slice(1);
+  };
+  for (let i = n.length - 1; i >= 0; i--) {
+    res += n[i]
+  };
+  return Number(res)
 }
